c314ef0a5e19b294b04cd6907535faa4337d3522
Wilhelm Mozes <wilhelmmozes@gmail.com>
2019-04-09 13:48:40 +0700

    add updateUser function

--- users.php
+++ users.php
@@ -25,4 +25,19 @@ function addUser($name,$pwd,$pos)
     mysqli_close($mysqli);
   }
 
+function updateUser($name,$pwd,$pos,$id)
+{
+    global $mysqli;
+    $sql = "UPDATE users SET user_name ='" . $name . "', user_password = '" . $pwd . "', user_position = '" . $pos . "' WHERE iduser = '" . $id . "'";
+    if (mysql_query($mysqli, $sql))
+    {
+        echo "Successfully updated user on user id " . $id;
+    }
+    else
+    {
+        echo "Error: " . $sql . "<br>" . mysqli_error($mysqli);
+    }
+    mysqli_close($mysqli);
+}
+
 ?>
